b03ae838eade38f479a9a39594e119fd173728bf
darkshala321 <141640866+darkshala321@users.noreply.github.com>
2024-01-18 19:25:21 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import Header from './components/header/Header';
-import News from './components/news/News';
-import FeaturePosts from './components/featurepost/FeaturePosts'
-import DailyPost from './components/dailypost/DailyPost';
+import SinglePost from './components/singlepost/SinglePost';
+import AuthorProfile from './components/page/AuthorProfile';
+import Home from './components/page/home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <News />
-      <FeaturePosts />
-      <DailyPost />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:_id" element={<SinglePost />} />
+          <Route path="/author/:_id" element={<AuthorProfile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
